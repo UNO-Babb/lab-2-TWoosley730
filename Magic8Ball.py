fa9c5702a08aef16154e4105dee812361ca1256c
TWoosley730 <twoosley@unomaha.edu>
2025-02-02 17:00:36 -0600

    Update Magic8Ball.py

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,9 +10,16 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  answers = ["thing 1", "thing 2"]
   #Answer question randomly with one of the options from your earlier list.
+  length = len(answers)
+  r = random.random() * length
 
+  r = int(r)
+  
+  print(r)
+  response = answers[r]
+  print(response)
 
 if __name__ == '__main__':
   main()
